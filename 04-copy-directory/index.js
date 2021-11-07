e8d89bec23874bed96b3302adf18e4d89f54a1c8
0deyal0 <dimanvks@gmail.com>
2021-11-07 17:31:42 +0300

    feat: resolve copy dir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
 
+const { promises: fs } = require("fs")
+const path = require("path")
+
+const copyDir = async (src = path.resolve(__dirname, 'files'), dest = path.resolve(__dirname, 'files-copy')) => {
+    await fs.mkdir(dest, { recursive: true });
+    let entries = await fs.readdir(src, { withFileTypes: true });
+
+    for (let entry of entries) {
+        let srcPath = path.join(src, entry.name);
+        let destPath = path.join(dest, entry.name);
+
+        entry.isDirectory() ?
+            await copyDir(srcPath, destPath) :
+            await fs.copyFile(srcPath, destPath);
+    }
+}
+
+exports.copyDir = copyDir;
+copyDir();
